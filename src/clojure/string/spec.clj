1fb67af31b076a20808fa2c5ac66cfd315550f2f
Quan Nguyen <huu2uan@gmail.com>
2017-11-29 21:46:05 +0700

    spec'ed

--- src/clojure/string/spec.clj
+++ src/clojure/string/spec.clj
@@ -5,13 +5,24 @@
 
 (defn- pattern? [p] (instance? Pattern p))
 
+(s/fdef clojure.string/reverse
+        :args (s/cat :s string?)
+        :ret string?
+        :fn (s/and #(= (count (:ret %)) (count (-> % :args :s)))
+                   #(= (clojure.string/reverse (:ret %)) (-> % :args :s))))
+
+(s/fdef clojure.string/re-quote-replacement
+        :args (s/cat :replacement string?)
+        :ret string?
+        :fn #(>= (count (:ret %)) (count (-> % :args :replacement))))
+
 (s/fdef clojure.string/replace
-        :args (s/alt :string-string (s/cat :s string?
-                                           :match string?
-                                           :replacement string?)
-                     :char-char (s/cat :s string?
+        :args (s/alt :char-char (s/cat :s string?
                                        :match char?
                                        :replacement char?)
+                     :string-string (s/cat :s string?
+                                           :match string?
+                                           :replacement string?)
                      :pattern-string (s/cat :s string?
                                             :match pattern?
                                             :replacement string?)
@@ -19,3 +30,111 @@
                                               :match pattern?
                                               :replacement ifn?))
         :ret string?)
+
+(s/fdef clojure.string/replace-first
+        :args (s/alt :char-char (s/cat :s string?
+                                       :match char?
+                                       :replacement char?)
+                     :string-string (s/cat :s string?
+                                           :match string?
+                                           :replacement string?)
+                     :pattern-string (s/cat :s string?
+                                            :match pattern?
+                                            :replacement string?)
+                     :pattern-function (s/cat :s string?
+                                              :match pattern?
+                                              :replacement ifn?))
+        :ret string?)
+
+(s/fdef clojure.string/join
+        :args (s/alt :coll (s/cat :coll coll?)
+                     :sep+coll (s/cat :separator any?
+                                      :coll coll?))
+        :ret string?)
+
+(s/fdef clojure.string/capitalize
+        :args (s/cat :s string?)
+        :ret string?
+        :fn #(= (count (:ret %)) (count (-> % :args :s))))
+
+(s/fdef clojure.string/upper-case
+        :args (s/cat :s string?)
+        :ret string?
+        :fn #(= (count (:ret %)) (count (-> % :args :s))))
+
+(s/fdef clojure.string/lower-case
+        :args (s/cat :s string?)
+        :ret string?
+        :fn #(= (count (:ret %)) (count (-> % :args :s))))
+
+(s/fdef clojure.string/split
+        :args (s/alt :re (s/cat :s string? :re pattern?)
+                     :re+limit (s/cat :s string? :re pattern? :limit int?))
+        :ret (s/coll-of string? :kind vector?))
+
+(s/fdef clojure.string/split-lines
+        :args (s/cat :s string?)
+        :ret (s/coll-of string? :kind vector?)
+        :fn #(<= (count (:ret %)) (count (-> % :args :s))))
+
+(s/fdef clojure.string/trim
+        :args (s/cat :s string?)
+        :ret string?
+        :fn #(<= (count (:ret %)) (count (-> % :args :s))))
+
+(s/fdef clojure.string/triml
+        :args (s/cat :s string?)
+        :ret string?
+        :fn #(<= (count (:ret %)) (count (-> % :args :s))))
+
+(s/fdef clojure.string/trimr
+        :args (s/cat :s string?)
+        :ret string?
+        :fn #(<= (count (:ret %)) (count (-> % :args :s))))
+
+(s/fdef clojure.string/trim-newline
+        :args (s/cat :s string?)
+        :ret string?
+        :fn #(<= (count (:ret %)) (count (-> % :args :s))))
+
+(s/fdef clojure.string/blank?
+        :args (s/cat :s string?)
+        :ret boolean?)
+
+(s/fdef clojure.string/escape
+        :args (s/cat :s string?
+                     :cmap (s/map-of char? any?))
+        :ret string?)
+
+(defn- str-or-char? [s] (or (string? s) (char? s)))
+
+(s/fdef clojure.string/index-of
+        :args (s/alt :idx (s/cat :s string?
+                                 :value str-or-char?)
+                     :idx+from (s/cat :s string?
+                                      :value str-or-char?
+                                      :from-index int?))
+        :ret (s/nilable int?))
+
+(s/fdef clojure.string/last-index-of
+        :args (s/alt :idx (s/cat :s string?
+                                 :value str-or-char?)
+                     :idx+from (s/cat :s string?
+                                      :value str-or-char?
+                                      :from-index int?))
+        :ret (s/nilable int?))
+
+(s/fdef clojure.string/starts-with?
+        :args (s/cat :s string?
+                     :substr string?)
+        :ret boolean?)
+
+(s/fdef clojure.string/ends-with?
+        :args (s/cat :s string?
+                     :substr string?)
+        :ret boolean?)
+
+(s/fdef clojure.string/includes?
+        :args (s/cat :s string?
+                     :substr string?)
+        :ret boolean?)
--- test/clojure/string/spec_test.clj
+++ test/clojure/string/spec_test.clj
@@ -1,34 +1,88 @@
 (ns clojure.string.spec-test
   (:require [clojure.string.spec]
             [clojure.spec.alpha :as s]
+            [clojure.spec.test.alpha :as stest]
             [expound.alpha :as expound]
             [orchestra.spec.test :as st]))
 
+;(stest/instrument)
 (st/instrument)
 (set! s/*explain-out* expound/printer)
 
-(clojure.string/replace 1 2 3)
+(clojure.string/reverse 123)
+(clojure.string/reverse "abc")
+
+(clojure.string/re-quote-replacement 123)
+(clojure.string/re-quote-replacement "abc")
+(clojure.string/re-quote-replacement "a\\bc$1")
 
 (clojure.string/replace "1" 2 3)
+(clojure.string/replace-first "1" 2 3)
 
-(clojure.string/replace "1" "2" 3)
+(clojure.string/join 1)
+(clojure.string/join 1 2)
+(clojure.string/join 1 [2 3])
 
-(clojure.string/replace "1" "2" \a)
+(clojure.string/capitalize 1)
+(clojure.string/capitalize "heLLO")
 
-(clojure.string/replace "1" "2" "3")
+(clojure.string/upper-case 1)
+(clojure.string/upper-case "heLLO")
 
-(clojure.string/replace "abc" \a :b)
+(clojure.string/lower-case 1)
+(clojure.string/lower-case "heLLO")
 
-(clojure.string/replace "abc" \a "b")
+(clojure.string/split 1 2)
+(clojure.string/split "a b c" 2)
+(clojure.string/split "a b c" #"\s")
+(clojure.string/split "a b c" #"\s" "s")
 
-(clojure.string/replace "abc" \a \b)
+(clojure.string/split-lines 1)
+(clojure.string/split-lines "a")
+(clojure.string/split-lines "a\n1")
 
-(clojure.string/replace "abc" #"a" \b)
+(clojure.string/trim 1)
+(clojure.string/trim " a  ")
 
-(clojure.string/replace "abc" #"a" "b")
+(clojure.string/triml 1)
+(clojure.string/triml " a  ")
 
-(clojure.string/replace "abc" #"a" {"a" "b"})
+(clojure.string/trimr 1)
+(clojure.string/trimr " a  ")
 
-(clojure.string/replace "abc" #"a" #(str % %))
+(clojure.string/trim-newline 1)
+(clojure.string/trim-newline " a\r\n")
 
-(clojure.string/replace "abc" #"a" (fn [s] (str s s)))
+(clojure.string/blank? 1)
+(clojure.string/blank? "")
+
+(clojure.string/escape 1 2)
+(clojure.string/escape "a" 1)
+(clojure.string/escape "abc" {"a" 1})
+(clojure.string/escape "abc" {})
+(clojure.string/escape "abc" {\a 1})
+
+(clojure.string/index-of 1 2)
+(clojure.string/index-of "abc" 2)
+(clojure.string/index-of "abc" \a)
+(clojure.string/index-of "abc" 1 2)
+
+(clojure.string/last-index-of 1 2)
+(clojure.string/last-index-of "abc" 2)
+(clojure.string/last-index-of "abc" \a)
+(clojure.string/last-index-of "abc" 0 0)
+
+(clojure.string/starts-with? 1 2)
+(clojure.string/starts-with? "abc" 2)
+(clojure.string/starts-with? "abc" \a)
+(clojure.string/starts-with? "abc" "a")
+
+(clojure.string/ends-with? 1 2)
+(clojure.string/ends-with? "abc" 2)
+(clojure.string/ends-with? "abc" \a)
+(clojure.string/ends-with? "abc" "a")
+
+(clojure.string/includes? 1 2)
+(clojure.string/includes? "abc" 2)
+(clojure.string/includes? "abc" \a)
+(clojure.string/includes? "abc" "a")
